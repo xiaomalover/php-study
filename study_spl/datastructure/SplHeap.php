8a3a49896c88d558621e182ddf154cbe25328e15
xiaomalover <xiaomalover@gmail.com>
2016-11-04 13:43:21 +0800

    增加堆

--- study_spl/datastructure/SplHeap.php
+++ study_spl/datastructure/SplHeap.php
@@ -22,30 +22,52 @@
  * public bool valid ( void )
  * }
  */
-$heap = new SplMaxHeap(); # Ascending order
-$heap->insert('E');
-$heap->insert('B');
-$heap->insert('D');
-$heap->insert('A');
-$heap->insert('C');
 
-echo $heap->extract(), PHP_EOL; # E
-echo $heap->extract(), PHP_EOL; # D
+/**
+ * 由于SplHeap是抽象类，所以得继承后实现其抽象方法
+ */
+class JupilerLeague extends SplHeap
+{
+    /**
+     * 实现抽象方法
+     * 通过给定数组的值排序
+     */
+    public function compare($array1, $array2)
+    {
+        $values1 = array_values($array1);
+        $values2 = array_values($array2);
+        if ($values1[0] === $values2[0]) {
+            return 0;
+        }
+        return $values1[0] < $values2[0] ? -1 : 1;
+    }
+}
 
-$heap = new SplMinHeap(); # Descending order
-$heap->insert('E');
-$heap->insert('B');
-$heap->insert('D');
-$heap->insert('A');
-$heap->insert('C');
+// 插入数据到堆
+$heap = new JupilerLeague();
+$heap->insert(array('AA Gent' => 15));
+$heap->insert(array('Anderlecht' => 20));
+$heap->insert(array('Cercle Brugge' => 11));
+$heap->insert(array('Charleroi' => 12));
+$heap->insert(array('Club Brugge' => 21));
+$heap->insert(array('G. Beerschot' => 15));
+$heap->insert(array('Kortrijk' => 10));
+$heap->insert(array('KV Mechelen' => 18));
+$heap->insert(array('Lokeren' => 10));
+$heap->insert(array('Moeskroen' => 7));
+$heap->insert(array('Racing Genk' => 11));
+$heap->insert(array('Roeselare' => 6));
+$heap->insert(array('Standard' => 20));
+$heap->insert(array('STVV' => 17));
+$heap->insert(array('Westerlo' => 10));
+$heap->insert(array('Zulte Waregem' => 15));
 
-print PHP_EOL;
-echo $heap->extract(), PHP_EOL; # A
-echo $heap->extract(), PHP_EOL; # B
-$heap->rewind();
-$heap->next();
-echo $heap->key(), $heap->current();
-$heap->valid() && $heap->extract();
-$heap->valid() && $heap->extract();
-$heap->valid() && $heap->extract();
-echo $heap->isEmpty();
+// 到堆的顶部【第一个结点】
+$heap->top();
+
+// 打印堆中数据
+while ($heap->valid()) {
+    $current = $heap->current();
+    echo key($current) . ': ' . $current[key($current)] . PHP_EOL;
+    $heap->next();
+}
